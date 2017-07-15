ce966fdc74b4a12b44f7ed53294be15da6cd270c
DaniloDaniel <choopper_14_@hotmail.com>
2017-07-15 11:11:35 -0400

    EMP-23 se agrega migración, creando bd de nuevo

--- UTA.Emprendedoras/UTA.Emprendedoras.Clases/Publicacion.vb
+++ UTA.Emprendedoras/UTA.Emprendedoras.Clases/Publicacion.vb
@@ -3,7 +3,7 @@
     Public Property ID As Integer
     Public Property Titulo As String
     Public Property Descripcion As String
-    Public Property Foto As String
+    Public Property Foto As Byte()
     Public Property FechaPublicacion As Date
     Public Property Creador As Usuario
     Public Property PublicacionTipo As TipoPublicacion
